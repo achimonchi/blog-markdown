87f39d9009804080773c18c45ca3a2a3c5ad6dbd
Reyhan <achimonchi.reyhan@gmail.com>
2021-07-17 23:43:54 +0700

    add tailwindcss

---
+++ frontend/src/components/nav.svelte
@@ -0,0 +1,6 @@
+
+
+<nav>
+    <li>Home</li>
+    <li>About</li>
+</nav>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Layout from "./../components/Layout.svelte";
+import {blogs, fetchBlog} from "./../stores/blog.store"
+
+import { onMount } from 'svelte';
+
+console.log($blogs)
+
+onMount(async()=>{
+    const data = await fetchBlog();
+    blogs.set(data);
+})
+
+</script>
+<Layout>
+    <h1 class="text-4xl">Home</h1>
+    {#each $blogs as blog }
+        <h1 class="text-2xl">{blog.title}</h1>
+        <p class="text-4xl">{blog.desc}</p>
+    {/each}
+</Layout>
